6f571a4e886b534a9dafd9491388a3426c9bb891
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-02 23:23:10 +0530

    latest commit

--- redis.sh
+++ redis.sh
@@ -33,16 +33,20 @@ VALIDATE(){
 
 }
 
-dnf module disable redis -y &>> ${LOG_FILE}
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling default redis"
+
 dnf module enable redis:7 -y &>>$LOG_FILE
-dnf install redis -y &>> ${LOG_FILE}
+VALIDATE $? "enabling default redis"
+
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing enable the redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode /c\protected-mode no' /etc/redis/redis.conf
-
-VALIDATE $? "editing redis service"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
 
 systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling redis"
 
 systemctl start redis &>> ${LOG_FILE}
-VALIDATE $? "Enabling and starting service"
+VALIDATE $? "Starting redis"
