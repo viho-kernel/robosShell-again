65b2fa61c3a22ee616f281ed22b35b00f47b8443
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-02 21:00:58 +0530

    latest commit

--- creator.sh
+++ creator.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+C="\e[36m"
+M="\e[35m"
+N="\e[0m"
+
+
 HOSTED_ZONE="Z0738852208EFDOYXFTUB"
 ZONE_NAME="opsora.space"
 AMI_ID="ami-0220d79f3f480ecf5"
@@ -11,65 +20,71 @@ for name in "${services[@]}"
 do
   EXISTING_ID=$(
     aws ec2 describe-instances \
-    --filters "Name=tag:Name,Values=$name" \
+    --filters \
+    "Name=tag:Name,Values=$name" \
     "Name=instance-state-name,Values=running,pending" \
     --query 'Reservations[*].Instances[*].InstanceId' \
-    --output text)
+    --output text
+    )
 
   if [ -n "$EXISTING_ID" ];then
-      echo " ${name} instance already exist. Hence, skippping instance creation.."
+      echo -e " $G ${name} instance already exist. $Y Hence, skippping instance creation..$N"
   else
-       echo " ${name} instance is not there. Hence, creating the instance... "
-    if [ $name == "mongodb" ] || [ $name == "mysql" ] || [ $name == "shipping" ]; then
+       echo -e " $R ${name} instance is not there. $G Hence, creating the instance... "
+    if [ "$name" == "mongodb" ] || [ "$name" == "mysql" ] || [ "$name" == "shipping" ]; then
      INSTANCE_ID=$(
     aws ec2 run-instances \
-    --image-id $AMI_ID \
+    --image-id "$AMI_ID" \
     --instance-type t3.medium \
-    --security-group-ids $SG_ID \
+    --security-group-ids "$SG_ID" \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$name}]" \
     --query 'Instances[0].InstanceId' \
     --output text
      )
-    elif [ $name == "catalogue" ] || [ $name == "frontend" ] || [ $name == "redis" ] || [ $name == "user" ] || [ $name == "cart" ] || [ $name == "rabbitmq" ] || [ $name == "payment" ] || [ $name == "dispatch" ] ; then
+    elif [ "$name" == "catalogue" ] || [ "$name" == "frontend" ] || [ "$name" == "redis" ] || [ "$name" == "user" ] || [ "$name" == "cart" ] || [ "$name" == "rabbitmq" ] || [ "$name" == "payment" ] || [ "$name" == "dispatch" ] ; then
     INSTANCE_ID=$(
     aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type t3.micro \
-    --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$name}]" \
-    --query 'Instances[0].InstanceId' \
-    --output text
+    --image-id "$AMI_ID" \
+          --instance-type t3.micro \
+          --security-group-ids "$SG_ID" \
+          --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$name}]" \
+          --query 'Instances[0].InstanceId' \
+          --output text
     )
     fi
 
-    echo -e " ${name} is succesfully created :) "
+    echo -R "$G ${name} instance created successfully. Instance ID: $C $INSTANCE_ID $N"
+
     fi
 
-    if [ $name == "frontend" ]; then
+    if [ "$name" == "frontend" ]; then
     IP=$(
-    aws ec2 describe-instances \
-    --filters Name=tag:Name,Values=$name \
-    --query 'Reservations[*].Instances[*].PublicIpAddress' 
-    --output text)
-    RECORD_NAME="$ZONE_NAME"
+   aws ec2 describe-instances \
+        --filters Name=instance-id,Values="$INSTANCE_ID" \
+        --query 'Reservations[*].Instances[*].PublicIpAddress' \
+        --output text
+    )
+    RECORD_NAME="${ZONE_NAME}."
     else
     IP=$(
         aws ec2 describe-instances \
-    --filters Name=tag:Name,Values=$name \
-    --query 'Reservations[*].Instances[*].PrivateIpAddress' 
-    --output text)
-    RECORD_NAME="$name.$ZONE_NAME"
+        --filters Name=instance-id,Values="$INSTANCE_ID" \
+        --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+        --output text
+        )
+    RECORD_NAME="${name}.${ZONE_NAME}"
     fi
+    
     echo -e " IP Address of the instance ${name} is : ${IP} "
 
-    aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE} --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE" --change-batch '
     {
   "Comment": "Updating A record",
   "Changes": [
     {
       "Action": "UPSERT",
       "ResourceRecordSet": {
-        "Name": "'$RECORD_NAME'",
+        "Name": "'"$RECORD_NAME"'",
         "Type": "A",
         "TTL": 1,
         "ResourceRecords": [
@@ -83,8 +98,9 @@ do
 }
 
 '
+ echo "DNS record updated: ${RECORD_NAME} → ${IP}"
+ echo -e "$M ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$N"
 
-echo "Instance ${name} is succesfully created. And IP Address ${IP} is assigned to it also ${RECORD_NAME} has been updated"
-      
+
 
 done
